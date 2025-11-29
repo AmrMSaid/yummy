841428d5de865780dbcc3a8b90488f5e24af8b91
AmrMSaid <amrmohamed4422@gmail.com>
2025-11-29 14:49:19 +0200

    Refactor meal fetching and display logic; improve code readability and structure

--- js/index.js
+++ js/index.js
@@ -6,6 +6,13 @@ let navs = document.querySelectorAll(".nav-link");
 let loading = document.querySelector(".loading");
 let submitBtn;
 
+async function fetchData(endpoint) {
+  let response = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/${endpoint}`
+  );
+  return await response.json();
+}
+
 function home() {
   clear();
   searchByName("");
@@ -42,51 +49,52 @@ function clearForSearch() {
 
 function displayMeals(arr) {
   let temp = "";
+
   for (let i = 0; i < arr.length; i++) {
     temp += `
-        <div class="col">
+      <div class="col">
+        <div
+          onclick="getMealDetails('${arr[i].idMeal}')"
+          class="img-card overflow-hidden"
+          role="button"
+        >
+          <img
+            src="${arr[i].strMealThumb}"
+            class="card-img-top rounded-2"
+          />
           <div
-            onclick="getMealDetails('${arr[i].idMeal}')"
-            class="img-card overflow-hidden"
-            role="button"
+            class="layer bg-white bg-opacity-75 top-0 h-100 w-100 d-flex flex-column justify-content-center rounded-2"
           >
-            <img
-              src="${arr[i].strMealThumb}"
-              class="card-img-top rounded-2"
-            />
-            <div
-              class="layer bg-white bg-opacity-75 top-0 h-100 w-100 d-flex flex-column justify-content-center rounded-2"
-            >
-              <h2 class="text-black ms-2">${arr[i].strMeal}</h2>
-            </div>
+            <h2 class="text-black ms-2">${arr[i].strMeal}</h2>
           </div>
         </div>
-        `;
+      </div>
+    `;
   }
   meals.innerHTML = temp;
 }
 
 function displaySearch() {
   search.innerHTML = `
-  <div class="row g-4 mb-5">
-          <div class="col-md-6">
-            <input
-              onkeyup="searchByName(this.value)"
-              class="form-control bg-transparent text-white"
-              type="text"
-              placeholder="Search By Name"
-            />
-          </div>
-          <div class="col-md-6">
-            <input
-              onkeyup="searchByFirstLetter(this.value)"
-              maxlength="1"
-              class="form-control bg-transparent text-white"
-              type="text"
-              placeholder="Search By First Letter"
-            />
-          </div>
-        </div>
+    <div class="row g-4 mb-5">
+      <div class="col-md-6">
+        <input
+          onkeyup="searchByName(this.value)"
+          class="form-control bg-transparent text-white"
+          type="text"
+          placeholder="Search By Name"
+        />
+      </div>
+      <div class="col-md-6">
+        <input
+          onkeyup="searchByFirstLetter(this.value)"
+          maxlength="1"
+          class="form-control bg-transparent text-white"
+          type="text"
+          placeholder="Search By First Letter"
+        />
+      </div>
+    </div>
   `;
   clearForSearch();
 }
@@ -94,12 +102,11 @@ function displaySearch() {
 async function searchByName(input) {
   loading.classList.remove("d-none");
   clearForSearch();
-  let response = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`
-  );
-  response = await response.json();
-  if (response.meals) {
-    displayMeals(response.meals);
+
+  let data = await fetchData(`search.php?s=${input}`);
+
+  if (data.meals) {
+    displayMeals(data.meals);
   } else {
     displayMeals([]);
   }
@@ -109,17 +116,16 @@ async function searchByName(input) {
 async function searchByFirstLetter(input) {
   loading.classList.remove("d-none");
   clearForSearch();
+
   if (input == "") {
     input = "a";
   } else {
     ("");
   }
-  let response = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/search.php?f=${input}`
-  );
-  response = await response.json();
-  if (response.meals) {
-    displayMeals(response.meals);
+  let data = await fetchData(`search.php?f=${input}`);
+
+  if (data.meals) {
+    displayMeals(data.meals);
   } else {
     displayMeals([]);
   }
@@ -128,142 +134,135 @@ async function searchByFirstLetter(input) {
 
 async function getCategories() {
   LoadAndClear();
-  let response = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/categories.php`
-  );
-  response = await response.json();
-  displayCategories(response.categories);
+
+  let data = await fetchData(`categories.php`);
+  displayCategories(data.categories);
+
   loading.classList.add("d-none");
 }
 
 function displayCategories(arr) {
   let temp = "";
+
   for (let i = 0; i < arr.length; i++) {
     temp += `
-        <div class="col">
+      <div class="col">
+        <div
+          onclick="getCategoryMeals('${arr[i].strCategory}')"
+          class="img-card overflow-hidden"
+          role="button"
+        >
+          <img
+            src="${arr[i].strCategoryThumb}"
+            class="card-img-top rounded-2"
+          />
           <div
-            onclick="getCategoryMeals('${arr[i].strCategory}')"
-            class="img-card overflow-hidden"
-            role="button"
+            class="layer bg-white bg-opacity-75 top-0 h-100 w-100 d-flex flex-column justify-content-center rounded-2 text-center p-2"
           >
-            <img
-              src="${arr[i].strCategoryThumb}"
-              class="card-img-top rounded-2"
-            />
-            <div
-              class="layer bg-white bg-opacity-75 top-0 h-100 w-100 d-flex flex-column justify-content-center rounded-2 text-center p-2"
-            >
-              <h2 class="text-black ms-2">${arr[i].strCategory}</h2>
-              <p>
-                ${arr[i].strCategoryDescription
-                  .split(" ")
-                  .slice(0, 20)
-                  .join(" ")}
-              </p>
-            </div>
+            <h2 class="text-black ms-2">${arr[i].strCategory}</h2>
+            <p>
+              ${arr[i].strCategoryDescription.split(" ").slice(0, 20).join(" ")}
+            </p>
           </div>
         </div>
-        `;
+      </div>
+    `;
   }
   meals.innerHTML = temp;
 }
 
 async function getCategoryMeals(category) {
   LoadAndClear();
-  let response = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`
-  );
-  response = await response.json();
-  displayMeals(response.meals.slice(0, 20));
+
+  let data = await fetchData(`filter.php?c=${category}`);
+  displayMeals(data.meals.slice(0, 20));
+
   loading.classList.add("d-none");
 }
 
 async function getAreas() {
   LoadAndClear();
-  let respone = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/list.php?a=list`
-  );
-  respone = await respone.json();
-  displayAreas(respone.meals);
+
+  let data = await fetchData(`list.php?a=list`);
+  displayAreas(data.meals);
+
   loading.classList.add("d-none");
 }
 
 function displayAreas(arr) {
   let temp = "";
+
   for (let i = 0; i < arr.length; i++) {
     temp += `
-        <div class="col">
-          <div
-            onclick="getAreaMeals('${arr[i].strArea}')"
-            class="rounded-2 text-center text-white"
-            role="button"
-          >
-            <i class="fa-solid fa-house-laptop fa-4x"></i>
-            <h3>${arr[i].strArea}</h3>
-          </div>
+      <div class="col">
+        <div
+          onclick="getAreaMeals('${arr[i].strArea}')"
+          class="rounded-2 text-center text-white"
+          role="button"
+        >
+          <i class="fa-solid fa-house-laptop fa-4x"></i>
+          <h3>${arr[i].strArea}</h3>
         </div>
-        `;
+      </div>
+    `;
   }
   meals.innerHTML = temp;
 }
 
 async function getAreaMeals(area) {
   LoadAndClear();
-  let response = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/filter.php?a=${area}`
-  );
-  response = await response.json();
-  displayMeals(response.meals.slice(0, 20));
+
+  let data = await fetchData(`filter.php?a=${area}`);
+  displayMeals(data.meals.slice(0, 20));
+
   loading.classList.add("d-none");
 }
 
 async function getIngredients() {
   LoadAndClear();
-  let respone = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/list.php?i=list`
-  );
-  respone = await respone.json();
-  displayIngredients(respone.meals.slice(0, 20));
+
+  let data = await fetchData(`list.php?i=list`);
+  displayIngredients(data.meals.slice(0, 20));
+
   loading.classList.add("d-none");
 }
 
 function displayIngredients(arr) {
   let temp = "";
+
   for (let i = 0; i < arr.length; i++) {
     temp += `
-        <div class="col">
-          <div
-            onclick="getIngredientMeals('${arr[i].strIngredient}')"
-            class="rounded-2 text-center text-white"
-            role="button"
-          >
-            <i class="fa-solid fa-drumstick-bite fa-4x"></i>
-            <h3>${arr[i].strIngredient}</h3>
-            <p>${arr[i].strDescription.split(" ").slice(0, 20).join(" ")}</p>
-          </div>
+      <div class="col">
+        <div
+          onclick="getIngredientMeals('${arr[i].strIngredient}')"
+          class="rounded-2 text-center text-white"
+          role="button"
+        >
+          <i class="fa-solid fa-drumstick-bite fa-4x"></i>
+          <h3>${arr[i].strIngredient}</h3>
+          <p>${arr[i].strDescription.split(" ").slice(0, 20).join(" ")}</p>
         </div>
-        `;
+      </div>
+    `;
   }
   meals.innerHTML = temp;
 }
 
 async function getIngredientMeals(ingredients) {
   LoadAndClear();
-  let response = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredients}`
-  );
-  response = await response.json();
-  displayMeals(response.meals.slice(0, 20));
+
+  let data = await fetchData(`filter.php?i=${ingredients}`);
+  displayMeals(data.meals.slice(0, 20));
+
   loading.classList.add("d-none");
 }
 
 async function getMealDetails(mealID) {
   LoadAndClear();
-  let respone = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`
-  );
-  respone = await respone.json();
-  displayMealDetails(respone.meals[0]);
+
+  let data = await fetchData(`lookup.php?i=${mealID}`);
+  displayMealDetails(data.meals[0]);
+
   loading.classList.add("d-none");
 }
 
@@ -272,19 +271,29 @@ function displayMealDetails(meal) {
 
   for (let i = 1; i <= 20; i++) {
     if (meal[`strIngredient${i}`]) {
-      ingredients += `<div class="alert alert-info m-2 p-1" role="alert">
-                  ${meal[`strMeasure${i}`]} ${meal[`strIngredient${i}`]}
-                </div>`;
+      ingredients += `
+        <div class="alert alert-info m-2 p-1" role="alert">
+          ${meal[`strMeasure${i}`]} ${meal[`strIngredient${i}`]}
+        </div>
+      `;
     }
   }
   let tagsArr = meal.strTags?.split(",");
-  if (!tagsArr) tagsArr = [];
-  let tags = "";
+  let tags = ``;
 
-  for (let i = 0; i < tagsArr.length; i++) {
-    tags += `<div class="alert alert-light m-2 p-1 mb-5" role="alert">
-                  ${tagsArr[i]}
-                </div>`;
+  if (tagsArr) {
+    tags += `
+      <h3 class="mt-3"><span class="fw-bolder">Tags: </span></h3>
+      <div class="d-flex flex-wrap">
+    `;
+    for (let i = 0; i < tagsArr.length; i++) {
+      tags += `
+        <div class="alert alert-light m-2 p-1" role="alert">
+          ${tagsArr[i]}
+        </div>
+      `;
+    }
+    tags += `</div>`;
   }
   let temp = `
     <div class="col-md-4">
@@ -297,14 +306,13 @@ function displayMealDetails(meal) {
     <div class="col-md-8">
       <h2><span class="fw-bolder">Instructions</span></h2>
       <p>${meal.strInstructions}</p>
-      <h3><span class="fw-bolder">Area: </span>${meal.strArea}</h3>
-      <h3><span class="fw-bolder">Category: </span>${meal.strCategory}</h3>
+      <h3 class="mb-4"><span class="fw-bolder">Area: </span>${meal.strArea}</h3>
+      <h3 class="mb-4"><span class="fw-bolder">Category: </span>${meal.strCategory}</h3>
       <h3><span class="fw-bolder">Recipes: </span></h3>
       <div class="d-flex flex-wrap">${ingredients}</div>
-      <h3><span class="fw-bolder">Tags: </span></h3>
-      <div class="d-flex flex-wrap">${tags}</div>
-      <a class="btn btn-success m-1" href="${meal.strSource}" target="_blank">Source</a>
-      <a class="btn btn-danger m-1" href="${meal.strYoutube}" target="_blank">YouTube</a>
+      ${tags}
+      <a class="btn btn-success m-1 mt-5" href="${meal.strSource}" target="_blank">Source</a>
+      <a class="btn btn-danger m-1 mt-5" href="${meal.strYoutube}" target="_blank">YouTube</a>
     </div>  
   `;
   details.innerHTML = temp;
@@ -320,93 +328,94 @@ let repasswordInputFocus = false;
 function displayContact() {
   clear();
   contact.classList.remove("d-none");
+
   contact.innerHTML = `
-   <div class="container text-center">
-          <div class="row g-4">
-            <div class="col-md-6">
-              <input
-                type="text"
-                class="form-control bg-white text-black"
-                id="nameInput"
-                placeholder="Name"
-                onkeyup="validateInputs()"
-              />
-              <div class="alert alert-danger w-100 mt-2 d-none" id="nameAlert">
-                Special characters and numbers not allowed
-              </div>
-            </div>
-            <div class="col-md-6">
-              <input
-                type="email"
-                class="form-control bg-white text-black"
-                id="emailInput"
-                placeholder="Email"
-                onkeyup="validateInputs()"
-              />
-              <div class="alert alert-danger w-100 mt-2 d-none" id="emailAlert">
-                Email not valid *example@yyy.zzz
-              </div>
-            </div>
-            <div class="col-md-6">
-              <input
-                type="tel"
-                class="form-control bg-white text-black"
-                id="phoneInput"
-                placeholder="Phone"
-                onkeyup="validateInputs()"
-              />
-              <div class="alert alert-danger w-100 mt-2 d-none" id="phoneAlert">
-                Enter valid Phone Number
-              </div>
-            </div>
-            <div class="col-md-6">
-              <input
-                type="number"
-                class="form-control bg-white text-black"
-                id="ageInput"
-                placeholder="Age"
-                onkeyup="validateInputs()"
-              />
-              <div class="alert alert-danger w-100 mt-2 d-none" id="ageAlert">
-                Enter valid age
-              </div>
-            </div>
-            <div class="col-md-6">
-              <input
-                type="password"
-                class="form-control bg-white text-black"
-                id="passwordInput"
-                placeholder="Password"
-                onkeyup="validateInputs()"
-              />
-              <div
-                class="alert alert-danger w-100 mt-2 d-none"
-                id="passwordAlert"
-              >
-                Enter valid password *Minimum eight characters, at least one
-                letter and one number:*
-              </div>
-            </div>
-            <div class="col-md-6">
-              <input
-                type="password"
-                class="form-control bg-white text-black"
-                id="repasswordInput"
-                placeholder="Re-password"
-                onkeyup="validateInputs()"
-              />
-              <div
-                class="alert alert-danger w-100 mt-2 d-none"
-                id="repasswordAlert"
-              >
-                Enter valid repassword
-              </div>
-            </div>
+    <div class="container text-center">
+      <div class="row g-4">
+        <div class="col-md-6">
+          <input
+            type="text"
+            class="form-control bg-white text-black"
+            id="nameInput"
+            placeholder="Name"
+            onkeyup="validateInputs()"
+          />
+          <div class="alert alert-danger w-100 mt-2 d-none" id="nameAlert">
+            Special characters and numbers not allowed
           </div>
-          <button type="button" class="btn btn-outline-danger m-4" id="submitBtn" disabled>
-            Submit
-          </button>
         </div>
+        <div class="col-md-6">
+          <input
+            type="email"
+            class="form-control bg-white text-black"
+            id="emailInput"
+            placeholder="Email"
+            onkeyup="validateInputs()"
+          />
+          <div class="alert alert-danger w-100 mt-2 d-none" id="emailAlert">
+            Email not valid *example@yyy.zzz
+          </div>
+        </div>
+        <div class="col-md-6">
+          <input
+            type="tel"
+            class="form-control bg-white text-black"
+            id="phoneInput"
+            placeholder="Phone"
+            onkeyup="validateInputs()"
+          />
+          <div class="alert alert-danger w-100 mt-2 d-none" id="phoneAlert">
+            Enter valid Phone Number
+          </div>
+        </div>
+        <div class="col-md-6">
+          <input
+            type="number"
+            class="form-control bg-white text-black"
+            id="ageInput"
+            placeholder="Age"
+            onkeyup="validateInputs()"
+          />
+          <div class="alert alert-danger w-100 mt-2 d-none" id="ageAlert">
+            Enter valid age
+          </div>
+        </div>
+        <div class="col-md-6">
+          <input
+            type="password"
+            class="form-control bg-white text-black"
+            id="passwordInput"
+            placeholder="Password"
+            onkeyup="validateInputs()"
+          />
+          <div
+            class="alert alert-danger w-100 mt-2 d-none"
+            id="passwordAlert"
+          >
+            Enter valid password *Minimum eight characters, at least one
+            letter and one number:*
+          </div>
+        </div>
+        <div class="col-md-6">
+          <input
+            type="password"
+            class="form-control bg-white text-black"
+            id="repasswordInput"
+            placeholder="Re-password"
+            onkeyup="validateInputs()"
+          />
+          <div
+            class="alert alert-danger w-100 mt-2 d-none"
+            id="repasswordAlert"
+          >
+            Enter valid repassword
+          </div>
+        </div>
+      </div>
+      <button type="button" class="btn btn-outline-danger m-4" id="submitBtn" disabled>
+        Submit
+      </button>
+    </div>
   `;
   submitBtn = document.getElementById("submitBtn");
 
